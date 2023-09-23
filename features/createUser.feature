137f91b9233b6f9846001bc98271a30ad2fe95c2
Rogerio Nakayama <rogenakayama@gmail.com>
2023-09-23 13:21:09 -0400

    feature: add error cases to create user

--- features/createUser.feature
+++ features/createUser.feature
@@ -4,3 +4,17 @@ Feature: Create user
 		Given a valid user to create
 		When create a new user
 		Then a token should be generated for the new user
+
+	Scenario: Error to create user, baddly formated email
+		Given the new user has the username "user1"
+		And the new user has the email "user.email"
+		And the new user has the password "123"
+		When create a new user
+		Then an error should exist
+
+	Scenario: Error to create user, email aready in use
+		Given the new user has the username "user1"
+		And the new user has the email "root@mock.com"
+		And the new user has the password "123"
+		When create a new user
+		Then an error should exist
--- features/step_definitions/createUser.mjs
+++ features/step_definitions/createUser.mjs
@@ -4,16 +4,32 @@ import { createUserRequest } from "../requests/createUserRequest.mjs";
 import { expect } from "chai";
 
 Given("a valid user to create", function () {
-	this.email = defaultUser.email,
-	this.password = defaultUser.password,
-	this.username = defaultUser.username
+	(this.email = defaultUser.email),
+		(this.password = defaultUser.password),
+		(this.username = defaultUser.username);
+});
+
+Given("the new user has the username {string}", function (username) {
+	this.username = username
+});
+
+Given("the new user has the email {string}", function (email) {
+	this.email = email
+});
+
+Given("the new user has the password {string}", function (password) {
+	this.password = password
 });
 
 When("create a new user", async function () {
-	this.apiResponse = await createUserRequest(this.username, this.email, this.password);
+	this.apiResponse = await createUserRequest(
+		this.username,
+		this.email,
+		this.password
+	);
 });
 
-Then("a token should be generated for the new user", function() {
-	expect(this.apiResponse.status).to.be.eq(201)
-	expect(this.apiResponse.data.user.token).to.exist.and.not.be.empty
-})
+Then("a token should be generated for the new user", function () {
+	expect(this.apiResponse.status).to.be.eq(201);
+	expect(this.apiResponse.data.user.token).to.exist.and.not.be.empty;
+});
